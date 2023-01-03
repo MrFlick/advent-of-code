5a93aa805464f1b8e776b9b8424a1f80906a9422
Matthew Flickinger <mflickinger@generatebiomedicines.com>
2023-01-03 02:34:59 +0000

    Day 16 - 2022, Part 2

--- 2022/py/2022-Day16-Volcano.py
+++ 2022/py/2022-Day16-Volcano.py
@@ -44,8 +44,8 @@ class Distances:
                 heappush(q, (dist+1, x))
         return None
 
-def explore():
-    valves = get_data("2022-Day16.txt")
+def explore1():
+    valves = get_data("2022-Day16-test.txt")
     hot_valves = set([k for k, v in valves.items() if v.flow_rate>0])
     dist = Distances(valves)
 
@@ -69,7 +69,46 @@ def explore():
 
     return inner("AA")[0]
 
-#print("Part 1:", explore())
+print("Part 1:", explore1())
 # 1720 too low (error in distance calculation)
 # 2019 too high (tried different start)
 # 1845
+
+def explore2():
+    valves = get_data("2022-Day16.txt")
+    hot_valves = set([k for k, v in valves.items() if v.flow_rate>0])
+    dist = Distances(valves)
+    best = 0
+
+    def inner(mepos='AA', metime=26, elepos='AA', eletime=26, pressure=0):
+        nonlocal best
+        if metime >= eletime:
+            mover = 'me'
+            pos = mepos
+            time = metime
+        else:
+            mover = 'ele'
+            pos = elepos
+            time = eletime
+        if len(hot_valves) < 1:
+            best = max(best, pressure)
+        for v in [valves[v] for v in hot_valves]:
+            d = dist.lookup(pos, v.name)
+            if time < d+1:
+                best = max(best, pressure)
+                continue
+            hot_valves.remove(v.name)
+            if mover == "me":
+                result = inner(mepos = v.name, metime = time - d - 1, elepos = elepos, eletime = eletime, pressure = pressure + v.flow_rate * (time - d - 1))
+            else:
+                result = inner(mepos = mepos, metime = metime, elepos= v.name, eletime = time - d - 1, pressure = pressure + v.flow_rate * (time - d - 1))
+            if result > best:
+                best = result
+            hot_valves.add(v.name)
+
+        return best
+
+    return inner("AA")
+
+print(explore2())
+# Part 2: 2286 (Very slow!)
