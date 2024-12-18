c0d209453efed7062791e32ae4e97b4231d459a1
Matthew Flickinger <mflickinger@generatebiomedicines.com>
2024-12-17 21:47:54 -0500

    Day 15, Part 2 - 2024

--- 2024/py/2024-Day15-WarehouseWoes.py
+++ 2024/py/2024-Day15-WarehouseWoes.py
@@ -34,20 +34,38 @@ def get_input(filename):
             moves.extend(x for x in line.strip())
     return start, Grid(grid, r, c+1), moves
 
+def widen_grid(grid: Grid) -> Grid:
+    new_nodes = {}
+    for k, v in grid.nodes.items():
+        if v == "#":
+            new_nodes[Pair(k.r, k.c*2)] = "#"
+            new_nodes[Pair(k.r, k.c*2+1)] = "#"
+        elif v == "O":
+            new_nodes[Pair(k.r, k.c*2)] = "["
+            new_nodes[Pair(k.r, k.c*2+1)] = "]"
+    return Grid(new_nodes, grid.NR, grid.NC * 2)
+
 
 def draw_grid(grid: Grid):
     for r in range(grid.NR):
         print("".join([grid.nodes.get(Pair(r, c), ".") for c in range(grid.NC)]))
 
-delta = {
+velocity_lookup = {
     "^": Pair(-1, 0),
     "<": Pair(0, -1),
     ">": Pair(0, 1),
     "v": Pair(1, 0)
 }
 
+axis_lookup = {
+    "^": "y",
+    "<": "x",
+    ">": "x",
+    "v": "y"
+}
+
 def action(start: Pair, grid: Grid, move: str):
-    velocity = delta[move]
+    velocity = velocity_lookup[move]
     step = start.add(velocity)
     if step not in grid.nodes:
         # free space
@@ -69,10 +87,56 @@ def action(start: Pair, grid: Grid, move: str):
             return start
         path.append(step)
 
-def score(grid: Grid):
+def next_front(grid, front: set[Pair], move: str) -> set[Pair] | None:
+    velocity = velocity_lookup[move]
+    axis = axis_lookup[move]
+    next_front = set()
+    for pos in front:
+        step = pos.add(velocity)
+        if step not in grid.nodes:
+            # free space
+            continue
+        elif grid.nodes[step] == "#":
+            # wall
+            return None
+        # Box        
+        if axis == "x":
+            if step in front:
+                continue
+            next_front.add(step)
+            next_front.add(step.add(velocity))
+        else:
+            next_front.add(step)
+            if grid.nodes[step] == "[":
+                next_front.add(step.add(Pair(0,1)))
+            elif grid.nodes[step] == "]":
+                next_front.add(step.add(Pair(0,-1)))
+    return next_front
+
+def action2(start: Pair, grid: Grid, move: str):
+    velocity = velocity_lookup[move]
+    front = next_front(grid, {start}, move)
+    fronts = []
+    while True:
+        if front is None:
+            return start
+        if len(front) == 0:
+            break
+        fronts.append(front)
+        front = next_front(grid, front, move)
+    for front in reversed(fronts):
+        front = list(front) # got from set to list for stability
+        vals = [grid.nodes[x] for x in front]
+        for x in front:
+            del grid.nodes[x]
+        for p, v in zip(front, vals):
+            grid.nodes[p.add(velocity)] = v
+    return start.add(velocity)
+
+def score(grid: Grid, char="O"):
     result = 0
     for k, v in grid.nodes.items():
-        if v == "O":
+        if v == char:
             result += 100 * k.r + k.c
     return(result)
 
@@ -82,8 +146,23 @@ def part1(filename):
         pos = action(pos, grid, move)
     return(score(grid))
 
+def part2(filename):
+    pos, grid, moves = get_input(filename)
+    grid = widen_grid(grid)
+    pos = Pair(pos.r, pos.c*2)
+    draw_grid(grid)
+    print(pos)
+    for move in moves:
+        pos = action2(pos, grid, move)
+    return(score(grid, "["))
+
 assert(part1("2024-Day15-test1.txt") == 2028)
 assert(part1("2024-Day15-test2.txt") == 10092)
 
 print("Part 1:", part1("2024-Day15.txt"))
 # 1517819
+
+assert(part2("2024-Day15-test2.txt") == 9021)
+
+print("Part 2:", part2("2024-Day15.txt"))
+# 1538862
